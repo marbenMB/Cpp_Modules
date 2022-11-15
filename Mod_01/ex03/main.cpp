8969e3e81430be17741b9120a83c74da85c69950
MAR BEN <marouanebenbajja@gmail.com>
2022-11-15 03:00:26 +0100

    Mod 01 - Ex03 : Adding main ?? Exercice Should be verfied

--- Mod_01/ex03/main.cpp
+++ Mod_01/ex03/main.cpp
@@ -16,13 +16,13 @@
 
 int main()
 {
-	// {
-	// 	Weapon club = Weapon("crude spiked club");
-	// 	HumanA bob("Bob", club);
-	// 	bob.attack();
-	// 	club.setType("some other type of club");
-	// 	bob.attack();
-	// }
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
 	{
 		Weapon club = Weapon("crude spiked club");
 		HumanB jim("Jim");
