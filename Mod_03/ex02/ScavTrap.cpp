f4f5fba5583ebac5e3c84ad471f6ac93328e7787
marbenMB <mbenbajj@student.1337.ma>
2022-11-23 01:05:26 +0100

    Mod 03 - Ex02 : Adding files

---
+++ Mod_03/ex02/ScavTrap.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbenbajj <mbenbajj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 21:27:11 by mbenbajj          #+#    #+#             */
+/*   Updated: 2022/11/23 01:04:35 by mbenbajj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap()
+{
+	std::cout << "ScavTrap Default Constructor Called" << std::endl;
+}
+
+ScavTrap::~ScavTrap()
+{
+	std::cout << "ScavTrap Destructor Called" << std::endl;
+}
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name) 
+{
+	std::cout << "ScavTrap Constructor Called" << std::endl;
+}
+
+ScavTrap::ScavTrap(ScavTrap const &obj)
+{
+	std::cout << "Copy Constructor Called" << std::endl;
+	if (this != &obj)
+		*this = obj;
+}
+
+ScavTrap	&ScavTrap::operator= (const ScavTrap &obj)
+{
+	std::cout << "Copy Assignment Operator Called" << std::endl;
+	(void)obj;
+	return (*this);
+}
+
+void	ScavTrap::attack (std::string const &target)
+{
+	if (this->_energy && this->_hitPt)
+	{
+		std::cout << "ScavTrap " << this->_name << " attacks " << target << \
+				" , causing " << this->_damage << " points of damage!" << std::endl;
+		if (this->_damage < 10)
+			this->_damage++;
+		this->_energy--;
+	}
+	else
+		std::cout << "ScavTrap " << this->_name << " Can't do anything!" << std::endl;
+}
+
+void	ScavTrap::guardGate ()
+{
+	std::cout << "ScavTrap " << this->getName() << " is now in Gate Keeper mode!" << std::endl;
+}
---
+++ Mod_03/ex02/ScavTrap.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbenbajj <mbenbajj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 21:27:15 by mbenbajj          #+#    #+#             */
+/*   Updated: 2022/11/23 01:04:38 by mbenbajj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _ScavTrap_HPP_
+#define _ScavTrap_HPP_
+
+#include <iostream>
+#include "ClapTrap.hpp"
+
+// ******************************************************** //
+//                         CLASSES                         //
+// ****************************************************** //
+
+class	ScavTrap : public ClapTrap
+{
+	public	:
+		ScavTrap ();
+		ScavTrap (std::string name);
+		ScavTrap (ScavTrap const &obj);
+		~ScavTrap ();
+		ScavTrap &operator= (const ScavTrap &obj);
+
+		void	guardGate (void);
+		void	attack (std::string const &target);
+	private	:
+		//	private attributes.
+};
+
+// ******************************************************** //
+//                        FUNCTIONS                        //
+// ****************************************************** //
+
+#endif
