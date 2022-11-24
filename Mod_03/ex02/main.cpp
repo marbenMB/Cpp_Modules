4b96d74b9449aafaf550e7afbe20f0c63174bd6f
marbenMB <mbenbajj@student.1337.ma>
2022-11-24 18:21:33 +0100

    Mod 03 - Ex02 : Check & Verifications

--- Mod_03/ex02/main.cpp
+++ Mod_03/ex02/main.cpp
@@ -6,58 +6,44 @@
 /*   By: mbenbajj <mbenbajj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 01:04:44 by mbenbajj          #+#    #+#             */
-/*   Updated: 2022/11/24 03:23:32 by mbenbajj         ###   ########.fr       */
+/*   Updated: 2022/11/24 18:21:01 by mbenbajj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
 #include "ScavTrap.hpp"
 
-// int main()
-// {
-// 	ClapTrap	goko("Goko");
-// 	ScavTrap	vijita("Vijita");
-// 	FragTrap	fraizer("Fraizer");
-
-// 	goko.attack(vijita.getName());
-// 	vijita.takeDamage(goko.getDamage());
-	
-// 	vijita.beRepaired(2);
-// 	vijita.attack(goko.getName());
-// 	goko.takeDamage(vijita.getDamage());
-
-// 	goko.beRepaired(15);
-
-// 	fraizer.attack(goko.getName());
-// 	goko.takeDamage(fraizer.getDamage());
-
-// 	goko.clapTrapStat();
-// 	vijita.clapTrapStat();
-// 	fraizer.clapTrapStat();
-
-// 	fraizer.attack(vijita.getName());
-// 	vijita.takeDamage(fraizer.getDamage());
-// 	goko.attack(vijita.getName());
-// 	vijita.takeDamage(goko.getDamage());
-
-// 	goko.clapTrapStat();
-// 	vijita.clapTrapStat();
-// 	fraizer.clapTrapStat();
-// 	vijita.guardGate();
-// 	fraizer.highFivesGuys();
-// 	return (0);
-// }
-
 int main()
 {
-	ScavTrap	goko("Goko");
+	ClapTrap	goko("Goko");
+	ScavTrap	vijita("Vijita");
 	FragTrap	fraizer("Fraizer");
 
-
-	goko.attack(fraizer.getName());
-	fraizer.takeDamage(goko.getDamage());
-
 	goko.clapTrapStat();
+	vijita.clapTrapStat();
+
+	goko.attack(vijita.getName()); 						// goko lose 1 energy. damage increase +1.
+	goko.attack(vijita.getName()); 						// goko lose 1 energy. damage increase +1.
+	goko.attack(vijita.getName()); 						// goko lose 1 energy. damage increase +1.
+	vijita.takeDamage(goko.getDamage());				// vijita lose 2 hitPt. damage decrease -1.
+
+	vijita.attack(goko.getName());						// vijita lose 1 energy. damage increase +1.
+	goko.takeDamage(vijita.getDamage());				// goko lose 20 hitPt. damage decrease -1.
+	
+	vijita.guardGate();
+	goko.beRepaired(2);									// can't do anything.
+	
+	goko.clapTrapStat();
+	vijita.clapTrapStat();
+	fraizer.clapTrapStat();
+	
+	vijita.attack(fraizer.getName());
+	fraizer.takeDamage(vijita.getDamage());
+
+	fraizer.attack(vijita.getName());
+	vijita.takeDamage(fraizer.getDamage());
+
+	vijita.clapTrapStat();
 	fraizer.clapTrapStat();
 
 	return (0);
