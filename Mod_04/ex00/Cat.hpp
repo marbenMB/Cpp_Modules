f4f9472cc759f0679515d00a6871a31a4731f132
marbenMB <mbenbajj@student.1337.ma>
2022-11-26 17:23:14 +0100

    Mod 04 - Ex00 : Implementing Animal base class and Cat & Dog derived calss. Implementation of virtual functions to override base class member functions.

---
+++ Mod_04/ex00/Cat.hpp
@@ -0,0 +1,40 @@
+#ifndef _Cat_HPP_
+#define _Cat_HPP_
+
+#include <iostream>
+#include "Animal.hpp"
+
+// ******************************************************** //
+//                         CLASSES                         //
+// ****************************************************** //
+
+class	Cat : public Animal
+{
+	public	:
+		Cat ();
+		Cat (Cat const &obj);
+		~Cat ();
+		Cat &operator= (const Cat &obj);
+
+		virtual void makeSound (void) const;
+	private	:
+		//	private attributes.
+};
+
+class	WrongCat : public WrongAnimal
+{
+	public	:
+		WrongCat ();
+		WrongCat (WrongCat const &obj);
+		~WrongCat ();
+		WrongCat &operator= (const WrongCat &obj);
+
+		void makeSound (void) const;
+	private	:
+		//	private attributes.
+};
+// ******************************************************** //
+	//                        FUNCTIONS                        //
+	// ****************************************************** //
+
+#endif
---
+++ Mod_04/ex00/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbenbajj <mbenbajj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/26 15:34:18 by mbenbajj          #+#    #+#             */
+/*   Updated: 2022/11/26 17:21:57 by mbenbajj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+int main()
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+
+	const WrongAnimal *wrongMeta = new WrongCat();
+
+	wrongMeta->makeSound();
+	return (0);
+}
