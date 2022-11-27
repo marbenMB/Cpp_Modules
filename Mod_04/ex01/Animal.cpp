e726fd5f18a18bcb48c5ecd4d9adfd67ba0a8d9d
marbenMB <mbenbajj@student.1337.ma>
2022-11-27 19:02:24 +0100

    Mod 04 - Ex01 : Deleting WrongAniaml & WrongCat Class

--- Mod_04/ex01/Animal.cpp
+++ Mod_04/ex01/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: mbenbajj <mbenbajj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 15:34:16 by mbenbajj          #+#    #+#             */
-/*   Updated: 2022/11/26 17:19:51 by mbenbajj         ###   ########.fr       */
+/*   Updated: 2022/11/27 19:01:36 by mbenbajj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,34 +45,4 @@ void	Animal::makeSound (void) const
 std::string	Animal::getType (void) const
 {
 	return (this->type);
-}
-
-WrongAnimal::WrongAnimal() : type("WrongAnimal")
-{
-	std::cout << "WrongAnimal : Default Constructor Called" << std::endl;
-}
-
-WrongAnimal::~WrongAnimal()
-{
-	std::cout << "WrongAnimal : Destructor Called" << std::endl;
-}
-
-WrongAnimal::WrongAnimal(WrongAnimal const &obj)
-{
-	std::cout << "Copy Constructor Called" << std::endl;
-	if (this != &obj)
-		*this = obj;
-}
-
-WrongAnimal	&WrongAnimal::operator= (const WrongAnimal &obj)
-{
-	std::cout << "Copy Assignment Operator Called" << std::endl;
-	if (this != &obj)
-		this->type = obj.type;
-	return (*this);
-}
-
-void	WrongAnimal::makeSound (void) const
-{
-	std::cout << "WrongAnimal Sound ..." << std::endl;
 }
--- Mod_04/ex01/Cat.cpp
+++ Mod_04/ex01/Cat.cpp
@@ -37,36 +37,3 @@ void	Cat::makeSound (void) const
 {
 	std::cout << "Meow Meow ..." << std::endl;
 }
-
-WrongCat::WrongCat()
-{
-	std::cout << "WrongCat : Default Constructor Called" << std::endl;
-	type = "WrongCat";
-}
-
-WrongCat::~WrongCat()
-{
-	std::cout << "WrongCat : Destructor Called" << std::endl;
-}
-
-WrongCat::WrongCat(WrongCat const &obj)
-{
-	std::cout << "Copy Constructor Called" << std::endl;
-	if (this != &obj)
-		*this = obj;
-}
-
-WrongCat	&WrongCat::operator= (const WrongCat &obj)
-{
-	std::cout << "Copy Assignment Operator Called" << std::endl;
-	if (this != &obj)
-	{
-		this->type = obj.type;
-	}
-	return (*this);
-}
-
-void	WrongCat::makeSound (void) const
-{
-	std::cout << "Meow Meow ..." << std::endl;
-}
