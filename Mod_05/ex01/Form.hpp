f5bd2ab09412620fb1bdf94fd187cfdc955f709e
marbenMB <mbenbajj@student.1337.ma>
2022-11-30 02:05:14 +0100

    Mod 05 - Ex01 : Adding Files and implement some stuff ... Review is Required.

---
+++ Mod_05/ex01/Form.hpp
@@ -0,0 +1,44 @@
+#ifndef _Form_HPP_
+#define _Form_HPP_
+
+#include <iostream>
+#include "Bureaucrat.hpp"
+
+// ******************************************************** //
+//                         CLASSES                         //
+// ****************************************************** //
+
+class	Form
+{
+	public	:
+		Form ();
+		Form (std::string name);
+		Form (Form const &obj);
+		~Form ();
+		Form &operator= (const Form &obj);
+
+		bool		getSign (void) const;
+		int	const	getReq (void) const;
+		int	const	getExec (void) const;
+		std::string	const	getName (void) const;
+
+		class	GradeTooHighException : public std::exception {
+			virtual	const char* what() const throw();
+		};
+
+		class	GradeTooLowException : public std::exception {
+			virtual	const char* what() const throw();
+		};
+
+	private	:
+		std::string	const	_name;
+		bool				_isSigned;
+		int	const			_required;
+		int	const			_execute;
+};
+std::ostream	&operator<< (std::ostream &out, Form const &obj);
+// ******************************************************** //
+//                        FUNCTIONS                        //
+// ****************************************************** //
+
+#endif
