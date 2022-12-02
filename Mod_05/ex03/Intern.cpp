ae98735b89d1f9fe3cc90dfc7d5f44fe657d8763
marbenMB <mbenbajj@student.1337.ma>
2022-12-02 03:03:54 +0100

    Mod 05 - Ex03 : - Intern Class Implemented. - Need more test.

--- Mod_05/ex03/Intern.cpp
+++ Mod_05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: mbenbajj <mbenbajj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 09:31:10 by mbenbajj          #+#    #+#             */
-/*   Updated: 2022/12/01 09:31:11 by mbenbajj         ###   ########.fr       */
+/*   Updated: 2022/12/02 03:02:56 by mbenbajj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,35 @@ Intern::Intern(Intern const &obj)
 Intern	&Intern::operator= (const Intern &obj)
 {
 	std::cout << "Copy Assignment Operator Called" << std::endl;
-	if (this != &obj)
-	{
-		// ...
-	}
+	(void)obj;
 	return (*this);
+}
+
+AForm*	Intern::makeForm (std::string name, std::string target)
+{
+	std::string	forms[3] = {"Shrubbery Creation", "Robotomy Request", "Presidential Pardon"};
+	int			i;
+	
+	for (i = 0; i < 3; i++)
+		if (!name.compare(forms[i]))
+			break;
+	if (i < 3)
+		std::cout << "Inter Creates " << name << std::endl;
+	switch (i)
+	{
+	case 0 :
+		return (new ShrubberyCreationForm(target));
+		break;
+	case 1 :
+		return (new RobotomyRequestForm(target));
+		break;
+	case 2 :
+		return (new PresidentialPardonForm(target));
+		break;
+	default:
+		std::cout << "Form Not Available For Inter!!" << std::endl;
+		throw "No Form Was Created!!";
+		break;
+	}
+	return (NULL);
 }
