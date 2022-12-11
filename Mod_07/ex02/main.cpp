3fd6d7e245375235e3a4370265f89eee3623c223
MAR BEN <marouanebenbajja@gmail.com>
2022-12-11 07:43:03 +0100

    Mod 07 - Ex02 : -> Adding files.

---
+++ Mod_07/ex02/main.cpp
@@ -0,0 +1,12 @@
+#include "Array.hpp"
+
+int main()
+{
+	//	You Code
+	int	*a = new int();
+	std::cout << *a << std::endl;
+
+	std::cout << "******	MAR_BEN CREATION √	******" << std::endl;
+
+	return (0);
+}
