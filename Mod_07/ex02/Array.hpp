eb1d38ba840b5794111516274bf9781fed96c699
marbenMB <mbenbajj@student.1337.ma>
2022-12-11 09:01:02 +0100

    Mod 07 - Ex02 : -> Implementing Array Class Template. -> Overloading [] operator (that throws exception if index is out of bounds). -> Use std::invalid_argument() exception class that has customizable string as parameter.

--- Mod_07/ex02/Array.hpp
+++ Mod_07/ex02/Array.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbenbajj <mbenbajj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/11 07:49:06 by mbenbajj          #+#    #+#             */
+/*   Updated: 2022/12/11 08:39:50 by mbenbajj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef _Array_HPP_
 #define _Array_HPP_
 
@@ -11,15 +23,59 @@ template <class T>
 class	Array
 {
 	public	:
-		Array ();
-		Array (unsigned int n);
-		Array (Array const &obj);
-		~Array ();
-		Array &operator= (const Array &obj);
+		Array () // Create empty array.
+		{
+			_size = 0;
+			_arr = new T[_size];
+		}
+		
+		Array (unsigned int n)	// Create array with n element initialized by default.
+		{
+			_size = n;
+			_arr = new T[_size];
+			for (unsigned int i = 0; i < _size; i++)
+				_arr[i] = 0;
+		}
+		
+		Array (Array const &obj)	// Copy Constructor.
+		{
+			if (this != &obj)
+			{
+				_arr = new T[1];
+				*this = obj;
+			}
+		}
+
+		~Array ()	// Destructor.
+		{
+			delete [] _arr;
+		}
+		
+		Array &operator= (const Array &obj)	// Copy assignement operator overloade.
+		{
+			if (this != &obj)
+			{
+				delete [] _arr;
+				_size = obj._size;
+				_arr = new T[_size];
+				for (unsigned int i = 0; i < _size; i++)
+					_arr[i] = obj._arr[i];
+			}
+			return *this;
+		}
+
+		T	&operator[] (unsigned int index)
+		{
+			if (index >= _size)
+				throw std::invalid_argument("INDEX OUT OF ARRAY BOUNDS!!");
+			return _arr[index];
+		}
+
+		unsigned int	size (void) const { return _size; }
 
 	private	:
-		T*		_arr;
-		size_t	_size;
+		T*				_arr;
+		unsigned int	_size;
 };
 
 // ******************************************************** //
