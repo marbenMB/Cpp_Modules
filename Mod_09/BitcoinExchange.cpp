3b5dafd6005c0fa0583bd985f87f54b7be5dd8a9
marbenMB <mbenbajj@student.1337.ma>
2023-03-31 15:24:49 +0000

    Adding subject + Storing DataBase

--- Mod_09/BitcoinExchange.cpp
+++ Mod_09/BitcoinExchange.cpp
@@ -1,5 +1,7 @@
 #include "BitcoinExchange.hpp"
 
+//	??	************	CLASS	************	??	//
+
 BitcoinExchange::BitcoinExchange() {}
 
 BitcoinExchange::BitcoinExchange(std::string file) : _fileName(file) {}
@@ -18,7 +20,7 @@ BitcoinExchange	&BitcoinExchange::operator= (const BitcoinExchange &obj)
 	// std::cout << "Copy Assignment Operator Called" << std::endl;
 	if (this != &obj)
 	{
-		// ...
+		_fileName = obj._fileName;
 	}
 	return (*this);
 }
@@ -28,16 +30,44 @@ void	BitcoinExchange::printError (std::string msg, std::string arg)
 	std::cout << "Error : " << msg << arg << std::endl;
 }
 
-void	BitcoinExchange::stockDB (void) const
+void	BitcoinExchange::stockDB (void)
 {
 	std::ifstream	input;
 	std::string		line;
+	size_t			nFind;
+	int		hint = 1;
+	std::string		date;
+	float			conv;
 
-	input.open(DB_PATH, std::ios_base::in);
+	input = openFile(DB_PATH);
 
 	while (std::getline(input,	line))
 	{
-		line.append("\n");
-		std::cout << line;
+		if (hint)
+		{
+			hint--;
+			std::cout << line << std::endl;
+			continue;
+		}
+		nFind = line.find(',');
+		date = line.substr(0, nFind);
+		conv = atof(line.substr(nFind + 1, line.length() - (nFind)).c_str());
+		_DB.insert(std::make_pair(date, conv));
+		line.clear();
+	}
+}
+
+
+//	??	************	FUNCTIONS	************	??	//
+
+std::ifstream	openFile (std::string path)
+{
+	std::ifstream	input;
+
+	input.open(path, std::ios_base::in);
+	if (!input.is_open())
+	{
+		BitcoinExchange::printError(E_FILE, NO_ARG);
+		throw	std::runtime_error("");
 	}
 }
