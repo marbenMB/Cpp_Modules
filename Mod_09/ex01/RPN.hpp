b0ccbeb2de72863c557f8f59ce6aaff0e3e1cd7f
marbenMB <mbenbajj@student.1337.ma>
2023-04-02 03:41:59 +0000

    Adding Ex01 Files

---
+++ Mod_09/ex01/RPN.hpp
@@ -0,0 +1,26 @@
+#ifndef _RPN_HPP_
+#define _RPN_HPP_
+
+#include <iostream>
+
+// ******************************************************** //
+//                         CLASSES                         //
+// ****************************************************** //
+
+class	RPN
+{
+	public	:
+		RPN ();
+		RPN (RPN const &obj);
+		~RPN ();
+		RPN &operator= (const RPN &obj);
+
+	private	:
+		//	DataType	attributes.
+};
+
+// ******************************************************** //
+//                        FUNCTIONS                        //
+// ****************************************************** //
+
+#endif
