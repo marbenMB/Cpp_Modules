47ee7e7e478a29d11ae3a1ac98b791c91541ef59
marbenMB <mbenbajj@student.1337.ma>
2023-04-03 04:49:07 +0000

    Adding Mod_09 - ex02 files

---
+++ Mod_09/ex02/PmergeMe.hpp
@@ -0,0 +1,26 @@
+#ifndef _PmergeMe_HPP_
+#define _PmergeMe_HPP_
+
+#include <iostream>
+
+// ******************************************************** //
+//                         CLASSES                         //
+// ****************************************************** //
+
+class	PmergeMe
+{
+	public	:
+		PmergeMe ();
+		PmergeMe (PmergeMe const &obj);
+		~PmergeMe ();
+		PmergeMe &operator= (const PmergeMe &obj);
+
+	private	:
+		//	DataType	attributes.
+};
+
+// ******************************************************** //
+//                        FUNCTIONS                        //
+// ****************************************************** //
+
+#endif
