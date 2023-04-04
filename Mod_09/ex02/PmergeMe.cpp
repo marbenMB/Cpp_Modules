b4e7893f8562bf664ccdd3b462f275dbbd471a16
marbenMB <mbenbajj@student.1337.ma>
2023-04-04 00:43:06 +0000

    Trying to Implement Merge-Insert sorting algo

--- Mod_09/ex02/PmergeMe.cpp
+++ Mod_09/ex02/PmergeMe.cpp
@@ -1,29 +1,55 @@
 #include "PmergeMe.hpp"
 
+//?:	----	CLASSES:	----	:?//
+
 PmergeMe::PmergeMe()
 {
-	std::cout << "PmergeMe : Default Constructor Called" << std::endl;
+	// std::cout << "PmergeMe : Default Constructor Called" << std::endl;
+}
+
+PmergeMe::PmergeMe(int ac, char **tab)
+{
+	for (int i = 0; i < ac; i++)
+	{
+		if (!checkIsNum(tab[i]))
+			throw	std::runtime_error("Bad Argument!!!");
+		else
+			_vecDB.push_back(std::atoi(tab[i]));
+	}
+	for (std::vector<int>::iterator it = _vecDB.begin(); it != _vecDB.end(); it++)
+		_deqDB.push_back(*it);
+	std::cout << std::endl;
 }
 
 PmergeMe::~PmergeMe()
 {
-	std::cout << "PmergeMe : Destructor Called" << std::endl;
+	// std::cout << "PmergeMe : Destructor Called" << std::endl;
 }
 
 PmergeMe::PmergeMe(PmergeMe const &obj)
 {
-	std::cout << "Copy Constructor Called" << std::endl;
+	// std::cout << "Copy Constructor Called" << std::endl;
 	if (this != &obj)
 		*this = obj;
 }
 
 PmergeMe	&PmergeMe::operator= (const PmergeMe &obj)
 {
-	std::cout << "Copy Assignment Operator Called" << std::endl;
+	// std::cout << "Copy Assignment Operator Called" << std::endl;
 	if (this != &obj)
 	{
-		//	this->attributes = obj.attributes;
-		//	...
+		_vecDB = obj._vecDB;
+		_deqDB = obj._deqDB;
 	}
 	return (*this);
+}
+
+//?:	----	FUNCTIONS	----	:?//
+
+bool	checkIsNum (std::string strNum)
+{
+	for (int i = 0; i < (int)strNum.length(); i++)
+		if (!std::isdigit(strNum[i]))
+			return false;
+	return true;
 }
--- Mod_09/ex02/PmergeMe.hpp
+++ Mod_09/ex02/PmergeMe.hpp
@@ -2,6 +2,8 @@
 #define _PmergeMe_HPP_
 
 #include <iostream>
+#include <vector>
+#include <deque>
 
 // ******************************************************** //
 //                         CLASSES                         //
@@ -11,16 +13,24 @@ class	PmergeMe
 {
 	public	:
 		PmergeMe ();
+		PmergeMe (int ac, char **tab);
 		PmergeMe (PmergeMe const &obj);
 		~PmergeMe ();
 		PmergeMe &operator= (const PmergeMe &obj);
+		void	mergeVec();
+		void	insertVec();
+		void	mergeDeq();
+		void	insertDeq();
 
 	private	:
-		//	DataType	attributes.
+		std::vector<int>	_vecDB;
+		std::deque<int>		_deqDB;
 };
 
 // ******************************************************** //
 //                        FUNCTIONS                        //
 // ****************************************************** //
+
+bool	checkIsNum (std::string strNum);
 
 #endif
