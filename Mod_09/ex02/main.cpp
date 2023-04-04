4f9da402af1a77799c0f6a0ee21d272b4807f84b
marbenMB <mbenbajj@student.1337.ma>
2023-04-04 00:43:06 +0000

    Trying to Implement Merge-Insert sorting algo

--- Mod_09/ex02/main.cpp
+++ Mod_09/ex02/main.cpp
@@ -1,8 +1,20 @@
 #include "PmergeMe.hpp"
 
-int main()
+int main(int ac, char **av)
 {
-	//	You Code
+	if (ac < 2)
+	{
+		std::cerr << "No Args !!!" << std::endl;
+		return 1;
+	}
+	try {
+		PmergeMe	sort(ac - 1, &av[1]);
+
+		std::cout << "Good Args √" << std::endl;
+		
+	}	catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
 
 	std::cout << "******	MAR_BEN CREATION √	******" << std::endl;
 
